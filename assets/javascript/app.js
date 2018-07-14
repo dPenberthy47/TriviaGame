164d2cbc60c35d22342e452e5ed52284582f8709
David Penberthy <davidpenberthy47@gmail.com>
2018-07-13 19:06:56 -0500

    added to the score screen

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -54,7 +54,7 @@ function showScores() {
         gameOverHTML += "<img src='https://media.giphy.com/media/l0Iy294JIZv3s2FEs/giphy.gif'/>";
         
     } 
-
+    gameOverHTML += "<p style='color: #E0202C; fontSize: 1em;'> click the header to play again! </p>";
     var element = document.getElementById("quiz");
     element.innerHTML = gameOverHTML;
     
